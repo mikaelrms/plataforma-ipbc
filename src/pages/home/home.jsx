31f9d5782cc9c83374c83754b4f59fe314f50fd6
Mikael <mikaelc623@gmail.com>
2026-05-22 18:04:11 -0300

    feat: criação do ambiente e hosting do projeto

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,9 @@
+function Home () {
+    return (    
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Bem-vindo à Plataforma IPBC</h1>
+        </div>
+    )
+
+} 
+export default Home;
